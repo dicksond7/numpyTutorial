9f13e7a9dc6871328bf59441c6c5459d8740c081
David Dickson <47121278+dicksond7@users.noreply.github.com>
2020-11-11 01:36:07 -0600

    Numbers module existed in numpy so renamed file

---
+++ numpyTutorial.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+
+# Simple Array Definition
+a = np.array([1,2,3])
+
+# 2D Array
+b = np.array([[1, 2], [3, 4]])
+
+# 2D Array without implicit definition
+# Define dimmension of array with ndmin
+c = np.array([1, 2, 3, 4, 5], ndmin=2)
+
+# Defining Array with certain datatype inside
+d = np.array([1, 2, 3], dtype=float)
+
+
+
+
